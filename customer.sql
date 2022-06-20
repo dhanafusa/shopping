969f75bd76472fb14b1a8a4a6cd19c61f513939b
dhanafusa <hanabusa@sapporo-mirai.ac.jp>
2022-06-20 12:08:04 +0900

    customer table

---
+++ customer.sql
@@ -0,0 +1,12 @@
+-- Adminer 4.8.1 PostgreSQL 14.2 dump
+
+DROP TABLE IF EXISTS "customer";
+CREATE TABLE "public"."customer" (
+    "id" integer NOT NULL
+) WITH (oids = false);
+
+INSERT INTO "customer" ("id") VALUES
+(1),
+(2);
+
+-- 2022-06-20 11:56:55.600627+09
